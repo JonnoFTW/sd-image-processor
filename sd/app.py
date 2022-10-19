4e23812960d6d25adb7de552bb5847cc56bb8c43
Jonathan Mackenzie <jonmac1@gmail.com>
2022-10-19 17:13:51 +1100

    better error handling and heartbeats

--- sd/app.py
+++ sd/app.py
@@ -14,9 +14,13 @@ import torch
 from kombu import Connection, Producer
 from kombu.mixins import ConsumerMixin
 
-
 from sd.pipeline import StableDiffusionPipeline
 from sd.handler import Handler
+
+import logging
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.INFO)
 
 
 class Worker(Handler, ConsumerMixin):
@@ -41,47 +45,52 @@ class Worker(Handler, ConsumerMixin):
     def generate_image(self, message: kombu.Message):
         payload = message.payload
         print("Got message:", payload, message.headers)
+        message.ack()
         if not payload['prompt'].strip():
             print("Received message with no prompt")
             message.ack()
             return
         start = time.time()
         seed = int(start)
-        image = self.pipe(
-            prompt=payload['prompt'],
-            height=payload.get('height', 512),
-            width=payload.get('width', 512),
-            num_inference_steps=payload.get('steps', 50),
-            guidance_scale=payload.get('scale', 7.5),
-            negative_prompt=payload.get('negative_prompt'),
-            seed=payload.get('seed', seed)
-        ).images[0]
-        took = time.time() - start
+        try:
+            image = self.pipe(
+                prompt=payload['prompt'],
+                height=payload.get('height', 512),
+                width=payload.get('width', 512),
+                num_inference_steps=payload.get('steps', 50),
+                guidance_scale=payload.get('scale', 7.5),
+                negative_prompt=payload.get('negative_prompt'),
+                seed=payload.get('seed', seed)
+            ).images[0]
+            took = time.time() - start
 
-        buff = BytesIO()
-        meta_data = PngInfo()
-        meta_data.add_text('Generated-With', 'StableDiffusion-v-1-4')
-        for field in ('prompt', 'steps', 'scale', 'negative_prompt', 'seed'):
-            if payload.get(field):
-                meta_data.add_text(field, str(payload[field]))
+            buff = BytesIO()
+            meta_data = PngInfo()
+            meta_data.add_text('Generated-With', 'StableDiffusion-v-1-4')
+            for field in ('prompt', 'steps', 'scale', 'negative_prompt', 'seed'):
+                if payload.get(field):
+                    meta_data.add_text(field, str(payload[field]))
 
-        image.save(buff, format='png', pnginfo=meta_data)
+            image.save(buff, format='png', pnginfo=meta_data)
 
-        result = {
-            'result': {
-                'img_blob': base64.b64encode(buff.getvalue()).decode('ascii'),
-                'ext': 'png',
-                'prompt': payload['prompt'],
-                'negative_prompt': payload.get('negative_prompt'),
-                'duration': round(took, 2),
-                'height': payload.get('height'),
-                'width': payload.get('width'),
-                'steps': payload.get('steps'),
-                'scale': payload.get('scale'),
-                'seed': payload.get('seed', seed)
-            },
-            'headers': payload.get('headers')
-        }
+            result = {
+                'result': {
+                    'img_blob': base64.b64encode(buff.getvalue()).decode('ascii'),
+                    'ext': 'png',
+                    'prompt': payload['prompt'],
+                    'negative_prompt': payload.get('negative_prompt'),
+                    'duration': round(took, 2),
+                    'height': payload.get('height'),
+                    'width': payload.get('width'),
+                    'steps': payload.get('steps'),
+                    'scale': payload.get('scale'),
+                    'seed': payload.get('seed', seed)
+                },
+                'headers': payload.get('headers')
+            }
+        except Exception as exc:
+            result = {'error': str(exc), 'headers': payload.get('headers')}
+
         with self.connection.channel() as channel:
             producer = Producer(channel)
             print("publishing result to", self.exchange, 'routing_key=', message.headers['return-key'])
@@ -93,12 +102,10 @@ class Worker(Handler, ConsumerMixin):
                 retry=True
             )
 
-        message.ack()
-
 
 def run_worker(broker_url, pipe):
     print("connecting to", broker_url)
-    with Connection(broker_url) as conn:
+    with Connection(broker_url, heartbeat=60) as conn:
         print(' [x] Awaiting image requests')
         worker = Worker(conn, pipe)
         atexit.register(conn.release)
