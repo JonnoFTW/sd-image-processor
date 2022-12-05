54bb33d0626b0dda2c65c75eee03ca36615967fd
Jonathan Mackenzie <jonmac1@gmail.com>
2022-12-05 21:28:35 +1100

    updated to support latest StableDiffusion Use SD2 Use threads

--- sd/app.py
+++ sd/app.py
@@ -1,20 +1,23 @@
+import queue
 import time
 import asyncio
 import base64
 import os
 from io import BytesIO
 import atexit
+import threading
 
 from PIL.PngImagePlugin import PngInfo
 
 import kombu
 import torch
+from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 # import torchdynamo
 
 from kombu import Connection, Producer
 from kombu.mixins import ConsumerMixin
 
-from sd.pipeline import StableDiffusionPipeline
+# from sd.pipeline import StableDiffusionPipeline
 from sd.handler import Handler
 
 import logging
@@ -23,7 +26,8 @@ logging.basicConfig()
 logging.getLogger().setLevel(logging.INFO)
 
 # "CompVis/stable-diffusion-v1-4"
-MODEL_NAME = "runwayml/stable-diffusion-v1-5"
+# MODEL_NAME = "runwayml/stable-diffusion-v1-5"
+MODEL_NAME = "stabilityai/stable-diffusion-2"
 
 
 class Worker(Handler, ConsumerMixin):
@@ -31,45 +35,65 @@ class Worker(Handler, ConsumerMixin):
         super().__init__()
         self.connection = connection
         self.pipe = pipe
+        self.queue = queue.Queue()
+        self.running = True
+        self.worker = threading.Thread(target=self.process_queue)
+        self.worker.start()
 
     def get_consumers(self, Consumer, channel):
         print("Making consumer for queue", self.image_requests_queue.name)
         return [
             Consumer(
                 queues=[self.image_requests_queue],
-                on_message=self.generate_image,
+                on_message=self.enqueue_image_request,
                 accept=['json'],
-                auto_declare=True,
+                auto_declare=True
             )
         ]
 
+    def process_queue(self):
+        while self.running:
+            try:
+                message = self.queue.get(timeout=2)
+                self.generate_image(message)
+            except queue.Empty:
+                pass
+        print("Exited")
+
     # @torchdynamo.optimize("ofi")
     # @torchdynamo.optimize("fx2trt")
+    def enqueue_image_request(self, message: kombu.Message):
+        # print("Enqueuing request")
+        self.queue.put_nowait(message)
+
     def generate_image(self, message: kombu.Message):
         payload = message.payload
-        print("Got message:", payload, message.headers)
-        message.ack()
+        print("[→] Got message:", payload, message.headers)
         if not payload['prompt'].strip():
-            print("Received message with no prompt")
+            print(" [✘] Received message with no prompt")
             message.ack()
             return
         start = time.time()
         seed = int(start)
         try:
-            image = self.pipe(
-                prompt=payload['prompt'],
-                height=payload.get('height', 512),
-                width=payload.get('width', 512),
-                num_inference_steps=payload.get('steps', 50),
-                guidance_scale=payload.get('scale', 7.5),
-                negative_prompt=payload.get('negative_prompt'),
-                seed=payload.get('seed', seed)
-            ).images[0]
+            with torch.inference_mode():
+                height = payload.get('height', 768)
+                width = payload.get('width', 768)
+                generator = torch.Generator(device=_pipe.device).manual_seed(payload.get('seed', seed))
+                image = self.pipe(
+                    prompt=payload['prompt'],
+                    height=height,
+                    width=width,
+                    num_inference_steps=payload.get('steps', 50),
+                    guidance_scale=payload.get('scale', 7.5),
+                    negative_prompt=payload.get('negative_prompt'),
+                    generator=generator
+                ).images[0]
             took = time.time() - start
 
             buff = BytesIO()
             meta_data = PngInfo()
-            meta_data.add_text('Generated-With', 'StableDiffusion-v-1-4')
+            meta_data.add_text('Generated-With', MODEL_NAME)
             for field in ('prompt', 'steps', 'scale', 'negative_prompt', 'seed'):
                 if payload.get(field):
                     meta_data.add_text(field, str(payload[field]))
@@ -94,41 +118,55 @@ class Worker(Handler, ConsumerMixin):
         except Exception as exc:
             result = {'error': str(exc), 'headers': payload.get('headers')}
 
-        with self.connection.channel() as channel:
-            producer = Producer(channel)
-            print("publishing result to", self.exchange, 'routing_key=', message.headers['return-key'])
-            producer.publish(
-                result,
-                exchange=self.exchange,
-                routing_key=message.headers['return-key'],
-                serializer='json',
-                retry=True
-            )
+        with get_connection() as connection:
+            with connection.channel() as channel:
+                producer = Producer(channel)
+                # print("publishing result to", self.exchange, 'routing_key=', message.headers['return-key'])
+                producer.publish(
+                    result,
+                    exchange=self.exchange,
+                    routing_key=message.headers['return-key'],
+                    serializer='json',
+                    retry=True
+                )
+        message.ack()
 
 
-def run_worker(broker_url, pipe):
-    print("connecting to", broker_url)
-    with Connection(broker_url, heartbeat=60) as conn:
-        print(' [x] Awaiting image requests')
+def get_connection():
+    broker_url = os.getenv('BROKER_URL', 'amqp://localhost')
+    # print("connecting to", broker_url)
+    return Connection(broker_url, heartbeat=10)
+
+
+def run_worker(pipe):
+    with get_connection() as conn:
+        print(' [✓] Awaiting image requests')
         worker = Worker(conn, pipe)
-        atexit.register(conn.release)
+
+        def on_exit():
+            worker.running = False
+            worker.worker.join()
+            conn.release()
+
+        atexit.register(on_exit)
         asyncio.ensure_future(worker.run())
 
 
 if __name__ == "__main__":
-    print(" [x] Loading pipeline")
+    print(" [✓] Loading pipeline")
     _pipe = StableDiffusionPipeline.from_pretrained(
         MODEL_NAME,
-        torch_type=torch.float16,
-        revision="fp16"
-    ).to("cuda")
-    # disable safety check
-    _pipe.safety_checker = lambda images, **kwargs: (images,)
-    _pipe.enable_xformers_memory_efficient_attention()
+        torch_dtype=torch.float16,
+        revision="fp16",
+        safety_checker=None,
+        requires_safety_checker=False
+    ) # type: StableDiffusionPipeline
+    _pipe.scheduler = DPMSolverMultistepScheduler.from_config(_pipe.scheduler.config)
     _pipe.enable_attention_slicing()
-    _pipe = _pipe.to("cuda")
+    _pipe.enable_xformers_memory_efficient_attention()
+    _pipe.to("cuda")
 
     try:
-        run_worker(os.getenv('BROKER_URL', 'amqp://localhost'), _pipe)
+        run_worker(_pipe)
     except KeyboardInterrupt:
         exit("Quitting")
